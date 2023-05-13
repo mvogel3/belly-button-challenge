fd1cbd251a8fe2cecc317da9019e35ba2c11ff5f
meg vogel <maryelizabethgvogel@gmail.com>
2023-05-13 15:47:06 -0700

    empty plot

--- Challenge_Code/ChallengeFiles/static/js/app.js
+++ Challenge_Code/ChallengeFiles/static/js/app.js
@@ -9,4 +9,41 @@ d3.json(url).then(function(data) {
     console.log(data);
   });
 
-  
+  function buildMetadata() {
+    return d3.json(url).then((data) => {
+     let metadata = data.metadata;
+     let names = data.names;
+     let samples = data.samples;
+   
+     return { metadata, names, samples };
+    });
+   };
+   
+   function init() {
+    buildMetadata().then((data) => {
+      let trace = [{
+       x: data.samples.sample_values,
+       y: data.samples.otu_ids,
+       type: "bar"
+      }];
+
+      Plotly.newPlot("bar", trace);
+   })};
+
+   init();
+
+  //  d3.selectAll("#selDataset").on("change", getData);
+
+  //  function getData() {
+  //   let dropDown = d3.select("#selDataset");
+  //   let dataset = dropDown.property("value");
+  //   let value = [];
+  //  }
+    
+    // let layout = {
+    //   height: 600,
+    //   width: 800
+    // };
+    // Plotly.newPlot("bar", trace, layout)
+
+    
